80f362c6d5b4871be453fbf84031972583bdef3f
followmeimdesna <150117735+followmeimdesna@users.noreply.github.com>
2023-12-16 13:49:36 +0300

    Add files via upload

---
+++ praktika1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class SmsMessage
+{
+    private string messageText;
+    private double price;
+
+    public string MessageText
+    {
+        get { return messageText; }
+        set
+        {
+            if (value.Length > 150)
+            {
+                messageText = value.Substring(0, 150);
+            }
+            else
+            {
+                messageText = value;
+            }
+            CalculatePrice();
+        }
+    }
+
+    public double Price
+    {
+        get { return price; }
+    }
+
+    public SmsMessage(string text)
+    {
+        MessageText = text;
+    }
+
+    private void CalculatePrice()
+    {
+        int length = messageText.Length;
+        if (length <= 55)
+        {
+            price = 1.5;
+        }
+        else
+        {
+            int extraCharacters = length - 55;
+            price = 1.5 + (extraCharacters * 0.5);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сообщение:");
+        string text = Console.ReadLine();
+
+        SmsMessage sms = new SmsMessage(text);
+
+        Console.WriteLine($"Стоимость сообщения: {sms.Price} р.");
+    }
+}
---
+++ Пр2 Задание 2.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+class NumberManager
+{
+    private int a = 0;
+
+    public bool SetNumber(int number)
+    {
+        if (number == a + 1)
+        {
+            a = number;
+            return true;
+        }
+        else
+        {
+            a = 0;
+            return false;
+        }
+    }
+
+    public int GetExpectedNumber()
+    {
+        return a + 1;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NumberManager manager = new NumberManager();
+
+        while (true)
+        {
+            int expectedNumber = manager.GetExpectedNumber();
+            Console.Write("Введите число " + expectedNumber + ": ");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                if (manager.SetNumber(userInput))
+                {
+                    Console.WriteLine("Верно! Следующее число...");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Ожидается число " + expectedNumber + ". Начните сначала.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите корректное число.");
+            }
+        }
+    }
+}
